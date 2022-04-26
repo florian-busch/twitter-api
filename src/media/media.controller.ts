af67c33bd959111480327943a87a355032718e72
Florian Busch <flbusch@gmx.de>
2022-04-26 16:16:12 +0200

    added fileupload route

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { MediaService } from './media.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('media')
 export class MediaController {
   constructor(private mediaService: MediaService) {}
 
-  @Get()
-  testFunction() {
-    return this.mediaService.testFunction()
+  @Post('/upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: function (req, file, cb) {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, file.fieldname + '-' + uniqueSuffix + extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -2,8 +2,4 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MediaService {
-
-  testFunction() {
-    return 'Test läuft'
-  }
 }

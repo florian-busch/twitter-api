c9f1d1b5615021b02d910b150469281befa7bbe1
Florian Busch <flbusch@gmx.de>
2022-05-09 15:16:22 +0200

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
+
   try {
   const app = await NestFactory.create(AppModule, {
     abortOnError: false,
   });
+  app.enableCors();
   await app.listen(3000);
 } catch (err) {
   console.log(err)
